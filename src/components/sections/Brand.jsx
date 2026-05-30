31d146085ff53edda153cac9cb18dbaf5a40c3cb
Ruben Aguilar <ragui56@wgu.edu>
2026-05-29 23:38:49 -0700

    refactor brand logo

--- src/components/sections/Brand.jsx
+++ src/components/sections/Brand.jsx
@@ -2,6 +2,7 @@ import logo from "../../assets/brand/logo.png";
 import heading from "../../assets/brand/heading.png";
 import subheading from "../../assets/brand/subheading.png";
 import ColorPalette from "./ColorPalette";
+import BrandLogo from "./BrandLogo";
 
 const Brand = () => {
   return (
@@ -18,28 +19,7 @@ const Brand = () => {
         </p>
       </div>
       <div className="grid md:grid-cols-2 lg:grid-cols-[1.3fr_1.3fr_1fr] gap-8 xl:gap-10">
-        <div className="space-y-6 glass rounded-2xl">
-          <img src={logo} alt="salt and sol logo" className="rounded-t-2xl" />
-          <div className="px-6 pb-6 space-y-2">
-            <h3 className="text-2xl xl:text-3xl tracking-wide uppercase text-purple-700">
-              primary logo
-            </h3>
-            <div className="text-[#94A3B8]">
-              <p className="mb-5">
-                The Salt & Sol logo has custom serif word-mark that makes it
-                feel timeless yet, inviting, striking, a balance between refined
-                elegance and relaxed charm.
-              </p>
-              <p className="mb-6">
-                Above the name, the radiant sunburst symbolizes the abundant
-                Arizona sunshine and the welcoming energy that defines the
-                restaurant’s atmosphere. Together, the elements evoke a sense of
-                sun, salt, and soul.
-              </p>
-            </div>
-          </div>
-        </div>
-
+        <BrandLogo />
         <div className="space-y-6">
           <div className="space-y-6 glass rounded-2xl">
             <img src={heading} alt="" className="rounded-t-2xl" />
