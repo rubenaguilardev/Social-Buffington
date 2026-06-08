90e55477c54c29922ee4b2c5344688291f319921
Ruben Aguilar <ragui56@wgu.edu>
2026-06-07 17:01:45 -0700

    change image order, add lazy load

--- src/components/sections/BrandDesigns.jsx
+++ src/components/sections/BrandDesigns.jsx
@@ -20,7 +20,8 @@ const BrandDesigns = () => {
             key={index}
             src={img}
             alt="salt and sol design image"
-            className="h-130 xl:h-140 w-full object-cover object-top rounded-2xl cursor-zoom-in"
+            loading="lazy"
+            className={`h-130 xl:h-140 w-full object-cover object-top rounded-2xl cursor-zoom-in ${index === 2 ? "order-4 md:order-none" : ""}`}
             onClick={() => {
               setPhotoIndex(index);
               setOpen(true);
