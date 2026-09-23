f639c6bd6d49e7be2e69904db68a79c29042dd79
rubenaguilardev <ragui56@wgu.edu>
2026-09-22 23:28:18 -0700

    update

--- src/components/sections/BrandDesigns.jsx
+++ src/components/sections/BrandDesigns.jsx
@@ -20,7 +20,7 @@ const BrandDesigns = () => {
             src={img}
             alt="salt and sol design image"
             loading={index === 0 ? "eager" : "lazy"}
-            className={`h-130 xl:h-140 w-full object-cover object-top rounded-2xl cursor-zoom-in ${index === 2 ? "order-4 md:order-none" : ""}`}
+            className={`h-110 md:h-100 xl:h-130 2xl:h-140 w-full object-cover object-top rounded-2xl cursor-zoom-in ${index === 2 ? "order-4 md:order-none" : ""}`}
             onClick={() => {
               setPhotoIndex(index);
               setOpen(true);
